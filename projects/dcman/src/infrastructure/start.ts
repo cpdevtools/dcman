97039d362963258c76c4b58ed67ae0b8282d4d47
Dan Erdelt <erd@conceptual.ca>
2023-06-02 15:19:30 +0000

    lib

---
+++ projects/dcman/src/infrastructure/start.ts
@@ -0,0 +1,55 @@
+import Docker from "dockerode";
+import { initializeDNSService } from "./dns/dns-service";
+import { initializeProxyService } from "./proxy/proxy-service";
+
+async function initializeDockerSwarm() {
+  const docker = new Docker();
+  const info = await docker.info();
+
+  if (info.Swarm.LocalNodeState === "active") {
+    return;
+  }
+
+  console.info("Initializing Docker Swarm");
+  const swarmId = await docker.swarmInit({
+    ListenAddr: "0.0.0.0",
+    Spec: {
+      Name: "default",
+      Orchestration: {
+        TaskHistoryRetentionLimit: 1,
+      },
+      TaskDefaults: {
+        LogDriver: {
+          Name: "json-file",
+          Options: {
+            "max-file": "3",
+            "max-size": "10m",
+          },
+        },
+      },
+      EncryptionConfig: {
+        AutoLockManagers: false,
+      },
+    },
+  });
+
+  console.info("Docker Swarm initialized", swarmId);
+}
+
+async function getConfig(id: string) {
+  const docker = new Docker();
+  const cfg = docker.getConfig(id);
+  try {
+    return await cfg.inspect();
+  } catch {
+    return null;
+  }
+}
+
+export function startInfrastructure() {
+  return Promise.all([initializeDockerSwarm(), initializeDNSService(), initializeProxyService()]);
+}
+
+(async () => {
+  await startInfrastructure();
+})();

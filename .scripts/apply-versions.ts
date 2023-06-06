35773a2d464e51e8c88ece14300d8ab143d399ea
Dan Erdelt <erd@conceptual.ca>
2023-06-06 14:42:28 +0000

    versioning

---
+++ .scripts/apply-versions.ts
@@ -0,0 +1,31 @@
+import glob from "fast-glob";
+import { writeFile } from "fs/promises";
+
+console.log("Applying versions...");
+
+const mainPkg = require("../package.json");
+const mainVersion = mainPkg.version;
+
+function replaceVersion(depSection: { [key: string]: string } | undefined, version: string) {
+  if (depSection) {
+    for (const key in depSection) {
+      const value = depSection[key];
+      if (value === "0.0.0-PLACEHOLDER") {
+        console.log(`  ${key}: ${value} -> ${version}`);
+        depSection[key] = version;
+      }
+    }
+  }
+}
+
+(async () => {
+  const files = await glob("projects/*/package.json");
+  for (const file of files) {
+    const pkg = require(`../${file}`);
+    replaceVersion(pkg.dependencies, mainVersion);
+    replaceVersion(pkg.devDependencies, mainVersion);
+    replaceVersion(pkg.peerDependencies, mainVersion);
+    replaceVersion(pkg.optionalDependencies, mainVersion);
+    await writeFile(file, JSON.stringify(pkg, null, 2) + "\n");
+  }
+})();

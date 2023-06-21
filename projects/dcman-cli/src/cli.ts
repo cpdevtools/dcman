3fccfd77c18edaa829bb8eb0bb5cb0940d8550df
Dan Erdelt <erd@conceptual.ca>
2023-06-21 18:14:40 +0000

    dc-event commands

--- projects/dcman-cli/src/cli.ts
+++ projects/dcman-cli/src/cli.ts
@@ -6,18 +6,92 @@ import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 
 const args = yargs(hideBin(process.argv))
-  .command(
-    "on-dc-startup <devContainer>",
-    "On Dev Container Startup",
-    () => {
-      return yargs.positional("devContainer", {
-        describe: "dev container name",
-        demandOption: true,
-        type: "string",
-      });
-    },
-    async (yargs) => {
-      await startInfrastructure();
-    }
-  )
+  .scriptName("dcm")
+  .command("dc-event", "event callbacks", (yargs) => {
+    return yargs
+      .command(
+        "initialize <devContainer>",
+        "On Dev Container Initialize",
+        (yargs) => {
+          return yargs.positional("devContainer", {
+            describe: "dev container id",
+            demandOption: true,
+            type: "string",
+          });
+        },
+        async (yargs) => {
+          await startInfrastructure();
+        }
+      )
+      .command(
+        "onCreate <devContainer>",
+        "On Dev Container Create",
+        (yargs) => {
+          return yargs.positional("devContainer", {
+            describe: "dev container id",
+            demandOption: true,
+            type: "string",
+          });
+        },
+        async (yargs) => {
+          // await startInfrastructure();
+        }
+      )
+      .command(
+        "updateContent <devContainer>",
+        "On Dev Container updateContent",
+        (yargs) => {
+          return yargs.positional("devContainer", {
+            describe: "dev container id",
+            demandOption: true,
+            type: "string",
+          });
+        },
+        async (yargs) => {
+          // await startInfrastructure();
+        }
+      )
+      .command(
+        "postCreate <devContainer>",
+        "On Dev Container postCreate",
+        (yargs) => {
+          return yargs.positional("devContainer", {
+            describe: "dev container id",
+            demandOption: true,
+            type: "string",
+          });
+        },
+        async (yargs) => {
+          // await startInfrastructure();
+        }
+      )
+      .command(
+        "postStart <devContainer>",
+        "On Dev Container postStart",
+        (yargs) => {
+          return yargs.positional("devContainer", {
+            describe: "dev container id",
+            demandOption: true,
+            type: "string",
+          });
+        },
+        async (yargs) => {
+          // await startInfrastructure();
+        }
+      )
+      .command(
+        "postAttach <devContainer>",
+        "On Dev Container postAttach",
+        (yargs) => {
+          return yargs.positional("devContainer", {
+            describe: "dev container id",
+            demandOption: true,
+            type: "string",
+          });
+        },
+        async (yargs) => {
+          // await startInfrastructure();
+        }
+      );
+  })
   .parse();

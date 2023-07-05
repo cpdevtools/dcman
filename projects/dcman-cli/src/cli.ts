0399dc36ec4870b51340178d509bf6a372672bd7
Dan Erdelt <erd@conceptual.ca>
2023-07-05 14:48:19 +0000

    github init

--- projects/dcman-cli/src/cli.ts
+++ projects/dcman-cli/src/cli.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { ensureGithubLogin, startInfrastructure, syncDevContainer } from "@cpdevtools/dcman";
+import { checkGithubToken, ensureGithubLogin, startInfrastructure, syncDevContainer } from "@cpdevtools/dcman";
 
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
@@ -32,6 +32,7 @@ const args = yargs(hideBin(process.argv))
         async (yargs) => {
           await syncDevContainer(true);
           await startInfrastructure();
+          await ensureGithubLogin();
         }
       )
       .command(
--- projects/dcman/src/github/github-login.ts
+++ projects/dcman/src/github/github-login.ts
@@ -1,35 +1,36 @@
-import { envVars, githubLogin, exec } from "@cpdevtools/lib-node-utilities";
+import { envVars, githubLogin, exec, importInquirer, githubAuthStatus, dockerLogin } from "@cpdevtools/lib-node-utilities";
 
 import { simpleGit } from "simple-git";
 
 export async function ensureGithubLogin() {
+  console.log("ensureGithubLogin");
   await ensureUserInfo();
-
-  //const result = await exec(`echo "${getGithubToken()}" | gh auth login --with-token`);
-
-  // check that github token is valid
-  // if not, prompt for new token and recheck
+  const githubInfo = await checkGithubToken();
+  await dockerLogin("https://ghcr.io", githubInfo.username!, githubInfo.token!);
 }
 
 export async function ensureUserInfo() {
   await ensureUserGitName();
   await ensureUserGitEmail();
-  await ensureGithubToken();
 }
 
-export async function ensureUserGitName() {
+export async function ensureUserGitName(update: boolean = true) {
   const git = simpleGit();
-  const name = await git.getConfig("user.name");
-  if (!name) {
+  const nameConfig = await git.getConfig("user.name");
+  const name = nameConfig?.value;
+  if (!name || update) {
+    const inquirer = await importInquirer();
+
     let prompt: { name?: string } | undefined = undefined;
-    // while (!prompt?.name) {
-    //   // prompt = await inquirer.prompt({
-    //   //   type: 'input',
-    //   //   name: 'name',
-    //   //   message: 'Please enter your full name',
-    //   // });
-    // }
-    // await git.addConfig("user.name", prompt.name, false, "global");
+    while (!prompt?.name?.trim()) {
+      prompt = await inquirer.prompt({
+        type: "input",
+        name: "name",
+        message: "Please enter your full name:",
+        default: name,
+      });
+    }
+    await git.addConfig("user.name", prompt.name, false, "global");
   }
 }
 
@@ -38,16 +39,19 @@ export async function getUserGitName() {
   return await git.getConfig("user.name");
 }
 
-export async function ensureUserGitEmail() {
+export async function ensureUserGitEmail(update: boolean = true) {
   const git = simpleGit();
-  const email = await git.getConfig("user.email");
-  if (!email) {
+  const emailConfig = await git.getConfig("user.email");
+  const email = emailConfig?.value;
+  if (!email || update) {
     let prompt: { email?: string } | undefined = undefined;
-    while (!prompt?.email) {
+    const inquirer = await importInquirer();
+    while (!prompt?.email?.trim()) {
       prompt = await inquirer.prompt({
         type: "input",
         name: "email",
         message: "Please enter your email",
+        default: email,
       });
     }
     await git.addConfig("user.email", prompt.email, false, "global");
@@ -59,16 +63,18 @@ export async function getUserGitEmail() {
   return await git.getConfig("user.email");
 }
 
-export async function ensureGithubToken() {
+export async function ensureGithubToken(update: boolean = true) {
   const git = simpleGit();
   const token = process.env.GITHUB_TOKEN;
-  if (!token) {
+  if (!token || update) {
+    const inquirer = await importInquirer();
     let prompt: { token?: string } | undefined = undefined;
-    while (!prompt?.token) {
+    while (!prompt?.token?.trim()) {
       prompt = await inquirer.prompt({
         type: "input",
         name: "token",
         message: "Please enter your github token (PAT)",
+        default: token,
       });
     }
     await envVars("GITHUB_TOKEN", prompt.token);
@@ -78,4 +84,22 @@ export async function ensureGithubToken() {
 
 export function getGithubToken() {
   return process.env.GITHUB_TOKEN;
+}
+
+export async function checkGithubToken(update: boolean = true) {
+  let userInfo = undefined;
+  const token = getGithubToken();
+  while (!userInfo) {
+    if (!token || update) {
+      await ensureGithubToken(update);
+    }
+    try {
+      const result = await githubAuthStatus();
+      userInfo = result;
+    } catch (e) {
+      console.error((e as any).data);
+    }
+    update = true;
+  }
+  return userInfo;
 }

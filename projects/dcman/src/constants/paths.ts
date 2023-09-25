41b7f1182ff9bf145152a99c79b85fcf3dece881
Erd <myrddraall@users.noreply.github.com>
2023-09-25 13:15:17 -0400

    repos pathing

--- projects/dcman/src/constants/paths.ts
+++ projects/dcman/src/constants/paths.ts
@@ -10,4 +10,4 @@ export const DCM_CACHE_DIR = `${DCM_DIR}/.cache`;
 
 export const DEVCONTAINER_DIR = "/devcontainer";
 export const WORKSPACES_DIR = "/devcontainer/workspaces";
-export const PROJECTS_DIR = "/devcontainer/projects";
+export const REPOS_DIR = "/devcontainer/repos";
